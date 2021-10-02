fe20a4ae780bde8e1f69d976cc6c914d96e450cc
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-10-02 21:31:10 +0900

    Main Coding Page

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  test
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<!--template 안에 HTML 짜고, script 안에 JS 짜고, style 안에 CSS 짠다-->
